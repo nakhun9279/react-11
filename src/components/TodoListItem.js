34eb0d48c283aaa074436b960ae79df6270dc683
nakhun9279 <fjfj9279@naver.com>
2020-08-28 22:11:25 +0900

    10.3.1 App에서 Todos 상태 사용하기

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'allreact-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
+
+
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
+
+
+return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+
+
+export default TodoListItem;
